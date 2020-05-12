7f209e094862dda819a2ffb0abbf70f84ffda660
Michaël Arnauts <michael.arnauts@destiny.be>
2020-05-12 12:42:34 +0200

    Fix default

--- resources/lib/service.py
+++ resources/lib/service.py
@@ -46,7 +46,7 @@ class BackgroundService(Monitor):
     @staticmethod
     def _is_refresh_required():
         """ Returns if we should trigger an update based on the settings. """
-        refresh_interval = kodiutils.get_setting_int('refresh_interval', 24) * 60 * 60
+        refresh_interval = kodiutils.get_setting_int('refresh_interval', 24) * 3600
         last_refreshed = kodiutils.get_setting_int('last_refreshed', 0)
         return (last_refreshed + refresh_interval) <= time.time()
 
