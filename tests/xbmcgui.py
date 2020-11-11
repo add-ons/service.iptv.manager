44d70dc6cc2d43970eb52546a9d7511559c398da
Michaël Arnauts <michael.arnauts@gmail.com>
2020-11-11 11:33:21 +0100

    Allow passing credits with the EPG data.

--- tests/xbmcgui.py
+++ tests/xbmcgui.py
@@ -3,7 +3,7 @@
 # GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """This file implements the Kodi xbmcgui module, either using stubs or alternative functionality"""
 
-# pylint: disable=invalid-name,super-on-old-class,too-many-arguments,unused-argument,useless-super-delegation
+# pylint: disable=invalid-name,super-on-old-class,too-many-arguments,unused-argument,useless-super-delegation,too-few-public-methods
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
